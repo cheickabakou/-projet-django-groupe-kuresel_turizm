4156f28dd6de6d2366e875aeb6cd6b88a9347590
benda10 <brendamenga4@gmail.com>
2025-12-05 17:09:37 +0300

    feat(visite):visite uygulaması eklendi - ziyaret takip sistemi

---
+++ visite/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class VisiteConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'visite'
---
+++ visite/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'visite'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('destinations/', views.destinations, name='destinations'),
+]
